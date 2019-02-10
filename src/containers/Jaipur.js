8233984de67e058a31bbed602340d672fc3f7d0d
Justin Bell <jbell@circle.com>
2019-02-10 12:27:05 -0500

    Combine Jaipur container and component

--- src/containers/Jaipur.js
+++ src/containers/Jaipur.js
@@ -1,18 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
 
-import { Jaipur as JaipurComponent } from '../components/Jaipur'
+import Market from '../containers/Market.js';
+import Hand from '../containers/Hand.js';
+import Take from '../containers/Take.js';
+import Sell from '../containers/Sell.js';
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Market</h2>
+        <Market />
+        <h2>Hand</h2>
+        <Hand />
+        <Take />
+        <Sell />
+        <h2>Tokens</h2>
+      </div>
+    )
+  }
+
+  componentWillMount() {
+    const gameId = this.props.gameId;
+    this.props.client.joinGame(gameId);
+  }
+}
+
+Component.propTypes = {
+  gameId: PropTypes.string.isRequired,
+  client: PropTypes.object.isRequired, 
+}
 
 const mapStateToProps = (state, ownProps) => {
   return {
     gameId: ownProps.match.params.id,
-    joinGame: (id) => ownProps.client.joinGame(id)
+    client: ownProps.client
   }
 }
 
 const Jaipur = connect(
   mapStateToProps,
   null
-)(JaipurComponent)
-
+)(Component)
 
 export default Jaipur 
